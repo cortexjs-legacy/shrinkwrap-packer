ac5aead461e6349bc1898c6e810e57531bc591d4
supersheep <supersheepxjy@gmail.com>
2014-05-20 11:30:48 +0800

    split lib and middleware

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var express = require('express');
-var shrinkpacker = require('./lib/index.js');
+var shrinkpacker = require('./lib/middleware');
 var path = require('path');
 var errorhandler = require('errorhandler');
 
 var app = express();
-app.use('/mod',express.static('mod'));
+// app.use('/mod',express.static('mod'));
 
 app.use(shrinkpacker({
     root: path.join(__dirname,"mod"),
-    pack: path.join(__dirname,"packages") // default as @root
+    pack: path.join(__dirname,"packages")
 }));
 
 app.use(function(req,res){
     res.send(404,"not found");
 });
-app.use(errorhandler());
 
 app.listen(3000);
