e743daff91452664f37d1cf0eebb00bf249800aa
supersheep <supersheepxjy@gmail.com>
2014-05-22 16:53:35 +0800

    add err tests

--- test/fileparser.js
+++ test/fileparser.js
@@ -21,6 +21,15 @@ describe('get file list',function(){
     });
   });
 
+  it('full version without shrinkwrap',function(){
+    packer.fullVersionFiles({
+      name:"b",
+      version:"0.0.3"
+    },function(err,list){
+      expect(err).to.not.be.null;
+    });
+  });
+
   it('patch version',function(){
     packer.patchVersionFiles({
       name:"a",
@@ -36,6 +45,19 @@ describe('get file list',function(){
         'b/0.0.2/b.js',
         'b/0.0.2/b.min.js'
       ]);
+    });
+  });
+
+
+  it('patch version without shrinkwrap',function(){
+    packer.patchVersionFiles({
+      name:"a",
+      version:{
+        from:"0.1.0",
+        to:"0.1.9"
+      }
+    },function(err,list){
+      expect(err).to.not.be.null;
     });
   });
 });
