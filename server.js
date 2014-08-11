db4d8d19feceb5b7e913867c07dcfa5057f21f6b
supersheep <supersheepxjy@gmail.com>
2014-08-11 10:40:24 +0800

    add staticOnly config

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var CACHE = config.get("cache");
 var PACKPATH = config.get("packpath");
 var ROOTPATH = config.get("rootpath");
 var PORT = config.get("port");
+var STATIC_ONLY = config.get("staticOnly");
 
 if(CACHE){
   app.use('/zip', shrinkpacker.static({
@@ -16,11 +17,14 @@ if(CACHE){
     pack: PACKPATH
   }));
 }
+
+if(!STATIC_ONLY){
 app.use('/zip', shrinkpacker.dynamic({
   root: ROOTPATH,
   pack: PACKPATH,
   cache: CACHE
 }));
+}
 
 app.use(shrinkpacker.errorHandler);
 
